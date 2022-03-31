5fbdfb09fa3c3512b88412cda505409883f86fea
Danilo Moura <danilolmoura@gmail.com>
2022-03-31 18:16:27 -0300

    feature: handle update bar event

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Licenses
+  import Number.Currency
 
   def mount(_params, _session, socket) do
     IO.puts "MOUNT #{inspect(self())}"
@@ -20,20 +21,27 @@ defmodule LiveViewStudioWeb.LicenseLive do
           <div class="seats">
             <img src="images/license.svg">
             <span>
-              Your license is currently for <strong>2</strong> seats.
+              Your license is currently for <strong><%= @seats %></strong> seats.
             </span>
           </div>
 
-          <form>
-            <input type="range" min="1" max="10" name="seats" value="2" />
+          <form phx-change="update">
+            <input type="range" min="1" max="10" name="seats" value="<%= @seats %>" />
           </form>
 
           <div class="amount">
-            $40
+            <%= number_to_currency(@amount) %>
           </div>
         </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("update", %{"seats" => seats}, socket) do
+    seats = String.to_integer(seats)
+
+    socket = assign(socket, seats: seats, amount: Licenses.calculate(seats))
+    {:noreply, socket}
   end
 end
