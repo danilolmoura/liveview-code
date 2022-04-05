48252210ce2465761078de86b3c001cc9cb9dcce
Danilo Moura <danilolmoura@gmail.com>
2022-04-05 02:53:16 -0300

    feature: add refresh event

---
+++ lib/live_view_studio_web/live/sales_dashboard.ex
@@ -0,0 +1,63 @@
+defmodule LiveViewStudioWeb.SalesDashboardLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Sales
+
+  def mount(_params, _session, socket) do
+    IO.puts "MOUNT #{inspect(self())}"
+    socket = assign_stats(socket)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    IO.puts "RENDER #{inspect(self())}"
+
+    ~L"""
+    <h1>Sales Dashboard<h1>
+    <div id="dashboard">
+      <div class="stats">
+        <div class="stat">
+          <span class="value">
+            <%= @new_orders %>%
+          </span>
+          <span class="name">
+            New Orders
+          </span>
+        </div>
+
+        <div class="stat">
+          <span class="value">
+            <%= @sales_amount %>%
+          </span>
+          <span class="name">
+            Sales Amount
+          </span>
+        </div>
+
+        <div class="stat">
+          <span class="value">
+            <%= @satisfaction %>%
+          </span>
+          <span class="name">
+            Satisfaction
+          </span>
+        </div>
+      </div>
+
+      <button phx-click="refresh">
+        <img src="images/refresh.svg">
+        Refresh
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("refresh", _unsigned_params, socket) do
+    socket = assign_stats(socket)
+    {:noreply, socket}
+  end
+
+  defp assign_stats(socket) do
+    assign(socket, new_orders: Sales.new_orders(), sales_amount: Sales.sales_amount(), satisfaction: Sales.satisfation())
+  end
+end
